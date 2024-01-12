e72613320c1a00abdba345cb818537e103754b1f
Benjamin Sengupta <benjaminsengupta@gmail.com>
2024-01-12 10:58:15 -0500

    Working script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import mimetypes
 import os
 import sys
 from typing import TypedDict
@@ -24,17 +25,17 @@ class VideoMetadata:
 
 class VideoQuality:
     def __init__(
-        self, name: str, width: int, height: int, filename: str, ffmpeg_flags: str
+        self, name: str, width: int, height: int, filepath: str, ffmpeg_flags: str
     ):
         self.name = name
         self.width = width
         self.height = height
-        self.filename = filename
+        self.filepath = filepath
         self.ffmpeg_flags = ffmpeg_flags
 
 
 def download_input_mp4():
-    print("\n===== Downloading input =====")
+    print("\n===== Downloading input.mp4 =====")
 
     resp = requests.get(DOWNLOAD_URL)
     with open("input.mp4", "wb") as f:
@@ -64,6 +65,24 @@ def gather_metadata():
     return VideoMetadata(width, height, frame_rate, duration_seconds)
 
 
+def create_thumbnail():
+    print("\n===== Creating thumbnail =====")
+
+    command_parts = [
+        "./ffmpeg/ffmpeg",
+        "-i input.mp4",
+        "-ss 00:00:01.000",
+        "-vframes 1",
+        "-vf scale=320:180",
+        "out/thumbnail.jpg",
+    ]
+
+    command = " ".join(command_parts)
+
+    print(command)
+    os.system(command)
+
+
 def determine_qualities(metadata: VideoMetadata) -> list[VideoQuality]:
     print("\n===== Determining qualities =====")
 
@@ -73,24 +92,24 @@ def determine_qualities(metadata: VideoMetadata) -> list[VideoQuality]:
             name="1080p",
             width=1920,
             height=1080,
-            filename="1080p.mp4",
-            ffmpeg_flags="-vf scale=1920:1080 -b:a 192k",
+            filepath="out/1080p.mp4",
+            ffmpeg_flags="-vf scale=1920:1080",
         ),
         # 720p
         VideoQuality(
             name="720p",
             width=1280,
             height=720,
-            filename="720p.mp4",
-            ffmpeg_flags="-vf scale=1280:720 -b:a 128k",
+            filepath="out/720p.mp4",
+            ffmpeg_flags="-vf scale=1280:720",
         ),
         # 480p (minimum resolution)
         VideoQuality(
             name="480p",
             width=0,
             height=0,
-            filename="480p.mp4",
-            ffmpeg_flags="-vf scale=854:480 -b:a 96k",
+            filepath="out/480p.mp4",
+            ffmpeg_flags="-vf scale=854:480",
         ),
     ]
 
@@ -105,21 +124,27 @@ def determine_qualities(metadata: VideoMetadata) -> list[VideoQuality]:
 def run_transcode(metadata: VideoMetadata, qualities: list[VideoQuality]):
     print("\n===== Running transcode =====")
 
+    frame_rate = int(min(metadata.frame_rate, 60))
+
     common_params = [
         "-c:v h264",  # video codec H.264
         "-profile:v main",  # H.264 profile main
         "-level:v 4.0",  # H.264 level 4.0
-        "-c:a aac",  # audio codec AAC
+        "-an",  # remove audio
+        f"-r {frame_rate}",  # keep framerate (max 60fps)
+        f"-g {frame_rate * 2}",  # keyframe interval (max 2 seconds)
         "-crf 22",  # constant rate factor, lower is better quality
-        f"-r {min(metadata.frame_rate, 60)}",  # keep framerate (max 60fps)
         "-pix_fmt yuv420p",
-        "-movflags +faststart",
+        "-movflags faststart",
+        "-map_metadata -1",  # remove metadata
     ]
 
     def map_to_subcommand(q: VideoQuality):
-        return " ".join([q.ffmpeg_flags, *common_params, q.filename])
+        return " ".join([q.ffmpeg_flags, *common_params, q.filepath])
 
     subcommands = list(map(map_to_subcommand, qualities))
+
+    subcommands.append("-c:a aac -b:a 128k -map_metadata -1 out/audio.mp4")
 
     command = f"./ffmpeg/ffmpeg -i input.mp4 {' '.join(subcommands)}"
 
@@ -132,18 +157,17 @@ def generate_manifest(qualities: list[VideoQuality]):
     print("\n===== Generating manifest =====")
 
     def map_to_dash_stream(q: VideoQuality):
-        return (
-            f"input={q.filename},stream=audio,output=audio_{q.name}_dash.mp4,playlist_name=audio_{q.name}.m3u8,hls_group_id=audio,hls_name=ENGLISH "
-            + f"input={q.filename},stream=video,output=video_{q.name}_dash.mp4,playlist_name=video_{q.name}.m3u8,iframe_playlist_name=video_{q.name}_iframe.m3u8"
-        )
+        return f"input={q.filepath},stream=video,output=out/video_{q.name}.mp4,playlist_name=out/video_{q.name}.m3u8,iframe_playlist_name=out/video_{q.name}_iframe.m3u8"
 
     subcommands = list(map(map_to_dash_stream, qualities))
 
     command_parts = [
         "./shaka-packager/packager",
+        # audio stream
+        f"input=out/audio.mp4,stream=audio,output=out/audio.mp4,playlist_name=out/audio.m3u8,hls_group_id=audio,hls_name=ENGLISH",
         *subcommands,
-        "--hls_master_playlist_output hls_manifest.m3u8",
-        "--mpd_output dash_manifest.mpd",
+        "--hls_master_playlist_output out/manifest.m3u8",
+        "--mpd_output out/manifest.mpd",
     ]
 
     command = " ".join(command_parts)
@@ -152,9 +176,30 @@ def generate_manifest(qualities: list[VideoQuality]):
 
     os.system(command)
 
+    files_to_upload = [
+        "out/manifest.mpd",
+        "out/manifest.m3u8",
+        "out/thumbnail.jpg",
+        "out/audio.mp4",
+        "out/audio.m3u8",
+        *map(lambda q: f"out/video_{q.name}.mp4", qualities),
+        *map(lambda q: f"out/video_{q.name}.m3u8", qualities),
+        *map(lambda q: f"out/video_{q.name}_iframe.m3u8", qualities),
+    ]
 
-def get_file_size(filename: str):
-    return os.path.getsize(filename)
+    return files_to_upload
+
+
+def get_base_filename(filepath: str):
+    return filepath.split("/")[-1]
+
+
+def get_file_size(filepath: str):
+    return os.path.getsize(filepath)
+
+
+def get_file_mimetype(filepath: str):
+    return mimetypes.guess_type(filepath)[0]
 
 
 class CallbackPostResponse(TypedDict):
@@ -164,29 +209,33 @@ class CallbackPostResponse(TypedDict):
     part_size: int
 
 
-def upload_files(qualities: list[VideoQuality]):
+def upload_files(filepaths: list[str]):
     print("\n===== Uploading output =====")
 
-    print(list(map(lambda q: q.filename, qualities)))
+    print(filepaths)
 
-    for quality in qualities:
+    for filepath in filepaths:
         req_json = {
-            "quality_name": quality.name,
-            "content_type": "video/mp4",
-            "file_size": get_file_size(quality.filename),
+            "file_name": get_base_filename(filepath),
+            "file_size": get_file_size(filepath),
+            "content_type": get_file_mimetype(filepath),
         }
 
-        print("Requesting upload of", quality.filename, ":", req_json)
+        print("Requesting upload of", filepath, ":", req_json)
 
-        resp = requests.post(f"{CALLBACK_URL}/request-quality-upload", json=req_json)
+        resp = requests.post(f"{CALLBACK_URL}/request-file-upload", json=req_json)
+
+        if not resp.ok:
+            raise Exception(f"Upload request failed ({resp.status_code}): {resp.text}")
+
         resp_json: CallbackPostResponse = resp.json()
 
         part_data = []
 
-        with open(quality.filename, "rb") as f:
-            print("Reading", quality.filename)
+        with open(filepath, "rb") as f:
+            print("Reading", filepath)
             for idx, presigned_url in enumerate(resp_json["presigned_urls"]):
-                print("Uploading part", idx + 1, "of", quality.filename)
+                print("Uploading part", idx + 1, "of", filepath)
                 part_resp = requests.put(
                     presigned_url, data=f.read(resp_json["part_size"])
                 )
@@ -198,7 +247,7 @@ def upload_files(qualities: list[VideoQuality]):
                 )
 
         resp = requests.post(
-            f"{CALLBACK_URL}/finish-quality-upload",
+            f"{CALLBACK_URL}/finish-file-upload",
             json={
                 "upload_key": resp_json["upload_key"],
                 "multipart_upload_id": resp_json["multipart_upload_id"],
@@ -206,16 +255,15 @@ def upload_files(qualities: list[VideoQuality]):
             },
         )
 
-    # resp = requests.post(CALLBACK_URL, files=files)
-
 
 def main():
     # download_input_mp4()
+    # create_thumbnail()
     metadata = gather_metadata()
     qualities = determine_qualities(metadata)
-    # run_transcode(metadata, qualities)
-    generate_manifest(qualities)
-    # upload_files(qualities)
+    run_transcode(metadata, qualities)
+    files_to_upload = generate_manifest(qualities)
+    upload_files(files_to_upload)
 
 
 # Start script
